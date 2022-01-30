c299903271d1cb8cabff17b9c1797640cf9f7748
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-01-30 18:27:32 -0300

    Estudo comparativo de Constant e Enum

--- src/com/loiane/cursojava/aula53/Teste.java
+++ src/com/loiane/cursojava/aula53/Teste.java
@@ -6,6 +6,7 @@ public class Teste {
 		
 		
 		usandoEnum();
+		usandoConstant();
 
 	}
 	
@@ -18,6 +19,8 @@ public class Teste {
 		DiaSemana sabado = DiaSemana.SABADO;
 		DiaSemana domingo = DiaSemana.DOMINGO;
 		
+		System.out.println("\nUsando o conceito de Enum\n");
+
 		imprimeDiaSemana(segunda);
 		imprimeDiaSemana(terca);
 		imprimeDiaSemana(quarta);
@@ -29,32 +32,81 @@ public class Teste {
 	
 	public static void imprimeDiaSemana(DiaSemana dia) {
 		
+			switch (dia) {
+			case SEGUNDA:
+				System.out.println("Segunda-feira!");
+				break;
+			case TERCA:
+				System.out.println("Terça-feira!");
+				break;
+			case QUARTA:
+				System.out.println("Quarta-feira!");
+				break;
+			case QUINTA:
+				System.out.println("Quinta-feira!");
+				break;
+			case SEXTA:
+				System.out.println("Sexta-feira!");
+				break;
+			case SABADO:
+				System.out.println("Sabado-feira!");
+				break;
+			case DOMINGO:
+				System.out.println("Doming-feira!");
+				break;
+			default:
+				break;
+			}
+			
+		}
+	
+	public static void usandoConstant() {
+		int segunda = DiaSemanaConstant.SEGUNDA;
+		int terca = DiaSemanaConstant.TERCA;
+		int quarta = DiaSemanaConstant.QUARTA;
+		int quinta = DiaSemanaConstant.QUINTA;
+		int sexta = DiaSemanaConstant.SEXTA;
+		int sabado = DiaSemanaConstant.SABADO;
+		int domingo = DiaSemanaConstant.DOMINGO;
+		
+		
+		System.out.println("\nUsando o conceito de Constant\n");
+		imprimeDiaSemana(segunda);
+		imprimeDiaSemana(terca);
+		imprimeDiaSemana(quarta);
+		imprimeDiaSemana(quinta);
+		imprimeDiaSemana(sexta);
+		imprimeDiaSemana(sabado);
+		imprimeDiaSemana(domingo);
+	}
+	
+public static void imprimeDiaSemana(int dia) {
+		
 		switch (dia) {
-		case SEGUNDA:
+		case 1:
 			System.out.println("Segunda-feira!");
 			break;
-		case TERCA:
+		case 2:
 			System.out.println("Terça-feira!");
 			break;
-		case QUARTA:
+		case 3:
 			System.out.println("Quarta-feira!");
 			break;
-		case QUINTA:
+		case 4:
 			System.out.println("Quinta-feira!");
 			break;
-		case SEXTA:
+		case 5:
 			System.out.println("Sexta-feira!");
 			break;
-		case SABADO:
+		case 6:
 			System.out.println("Sabado-feira!");
 			break;
-		case DOMINGO:
+		case 7:
 			System.out.println("Doming-feira!");
 			break;
 		default:
 			break;
-		}
-		
+		}	
 	}
-	
 }
+

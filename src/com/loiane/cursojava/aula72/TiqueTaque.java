1358eb8e2cab59f64c487ff700bc59844a0f57dd
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-02-04 18:34:56 -0300

    Estudo dos métodos wait, notify e notifyAll em multithreads

---
+++ src/com/loiane/cursojava/aula72/TiqueTaque.java
@@ -0,0 +1,53 @@
+package com.loiane.cursojava.aula72;
+
+public class TiqueTaque {
+
+	boolean tique;
+
+	synchronized void tique(boolean estaExecutando) {
+
+		if(!estaExecutando) {
+			tique = true;
+			notify();
+			return;
+		}
+
+		System.out.print("Tique ");
+		tique = true;	
+		notify();
+
+
+		try {
+			while(tique) {
+				wait();
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	synchronized void taque(boolean estaExecutando) {
+
+		if(!estaExecutando) {
+			tique = false;
+			notify();
+			return;
+		}
+
+		System.out.println("Taque");
+		tique = false;	
+		notify();
+
+
+		try {
+			while(!tique) {
+				wait();
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
